448d7fca9ba546d19984a58e408e866ea14c5036
Johannes Stratmann <sn2010@onlinehome.de>
2021-09-23 22:11:41 +0200

    add GC9A01 display

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@
 
 #include "mbed.h"
 #include "lvgl/lvgl.h"
+#include "lvglDriver/LVGLDispDriver_GC9A01.h"
+
+SPI spiDisplay(PB_5, NC, PB_3);
 
 int main()
 {
     printf("Hello World, Mbed!\n");
 
     lv_init();
+    spiDisplay.frequency(40e6);
+    LVGLDispGC9A01* lvglDisplayGC9A01_1 = new LVGLDispGC9A01(spiDisplay, PB_9, PB_6, PB_7, PB_8);
 
     return 0;
 }
